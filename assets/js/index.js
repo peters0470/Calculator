fad0e4b8665cc1c78659f37ad44f62933cc74425
unknown <peters0470@gmail.com>
2021-06-08 08:31:53 -0500

    added operator function

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,40 @@ const calculator = {
     display.value = calculator.displayValue;
   }
 
+  function inputDigit(digit) {
+    const { displayValue } = calculator;
+    // Overwrite `displayValue` if the current value is '0' otherwise append to it
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  }
+
+  function inputDecimal(dot) {
+    // If the `displayValue` property does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+  }
+
+  function handleOperator(nextOperator) {
+    // Destructure the properties on the calculator object
+    const { firstOperand, displayValue, operator } = calculator
+    // `parseFloat` converts the string contents of `displayValue`
+    // to a floating-point number
+    const inputValue = parseFloat(displayValue);
+  
+    // verify that `firstOperand` is null and that the `inputValue`
+    // is not a `NaN` value
+    if (firstOperand === null && !isNaN(inputValue)) {
+      // Update the firstOperand property
+      calculator.firstOperand = inputValue;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+  
+  
+
   keys.addEventListener('click', (event) => {
     // Access the clicked element
     const { target } = event;
@@ -23,12 +57,14 @@ const calculator = {
     }
   
     if (target.classList.contains('operator')) {
-      console.log('operator', target.value);
+      handleOperator(target.value);
+      updateDisplay();
       return;
     }
   
     if (target.classList.contains('decimal')) {
-      console.log('decimal', target.value);
+      inputDecimal(target.value);
+      updateDisplay();
       return;
     }
   
@@ -36,9 +72,9 @@ const calculator = {
       console.log('clear', target.value);
       return;
     }
-  
-    console.log('digit', target.value);
+    
+    inputDigit(target.value);
+    updateDisplay();
   });
-  
-  updateDisplay();
+
   
